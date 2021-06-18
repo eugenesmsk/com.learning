40f3b538ea0abae574437f498c8df6e91697d541
eugenesmsk <41268173+eugenesmsk@users.noreply.github.com>
2021-06-18 12:41:17 +0300

    Create TwoSumRecursion.java
    Не особо быстрый способ

---
+++ src/main/java/com/mytasks/leetcode/TwoSumRecursion.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class TwoSumRecursion {
+    
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> numList = getNumsFromTree(root);
+        int firstNum;
+        int secondNum;
+        for(int i = 0; i < numList.size(); i++) {
+            firstNum = numList.get(i);
+            for(int j = numList.size() - 1; j >= 0; j--) {
+                if(i == j) continue;
+                secondNum = numList.get(j);
+                if(firstNum + secondNum == k) {
+                    return true;
+                }
+                
+            }
+        }
+        return false;
+    }
+    
+    public List<Integer> getNumsFromTree(TreeNode root) {
+        ArrayList<Integer> numList = new ArrayList<>();
+        addToList(numList, root);
+        return numList;
+        
+    }
+    
+    void addToList(ArrayList<Integer> list, TreeNode root) {
+        if(root.left != null) {addToList(list, root.left);}
+        list.add(root.val);
+        if(root.right != null) {addToList(list, root.right);}
+    }
+}
